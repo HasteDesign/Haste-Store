7e1d3bedaeb1f1bb8a36e009273ee46d56c6ffde
anyssa <anyssa@hastedesign.com.br>
2017-01-01 19:46:50 -0200

    Mudança de nome e remodelagem do widget para servir como módulo de shortcode geral

--- wp-content/themes/harness-store/inc/widgets/class-haste-shortcode-module.php
+++ wp-content/themes/harness-store/inc/widgets/class-haste-shortcode-module.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Haste_Woo_Products class.
+ * Haste_Shortcode_Module class.
  *
  * Facebook like widget.
  *
@@ -9,14 +9,14 @@
  * @author   WPBrasil
  * @version  2.2.0
  */
-class Haste_Woo_Products extends WP_Widget {
+class Haste_Shortcode_Module extends WP_Widget {
 
 	/**
 	 * Register widget with WordPress.
 	 */
 	public function __construct() {
 		parent::__construct(
-			'haste_woo_products',
+			'haste_shortcode_module',
 			__( 'Haste Woo Products', 'harness-store' ),
 			array( 'description' => __( 'Displays WooCommerce products', 'harness-store' ), )
 		);
@@ -32,8 +32,9 @@ class Haste_Woo_Products extends WP_Widget {
 	 * @return string          Widget options form.
 	 */
 	public function form( $instance ) {
-		$title         = isset( $instance['title'] ) ? $instance['title'] : __('Featured products', 'harness-store');
+		$title         = isset( $instance['title'] ) ? $instance['title'] : __('My shortcode', 'harness-store');
 		$subtitle      = isset( $instance['subtitle'] ) ? $instance['subtitle'] : '';
+		$content       = isset( $instance['content'] ) ? $instance['content'] : '';
 		$shortcode     = isset( $instance['shortcode'] ) ? $instance['shortcode'] : '';
 
 		?>
@@ -47,6 +48,12 @@ class Haste_Woo_Products extends WP_Widget {
 			<label for="<?php echo $this->get_field_id( 'subtitle' ); ?>">
 				<?php _e( 'Subtitle', 'harness-store' ); ?>
 				<input id="<?php echo $this->get_field_id( 'subtitle' ); ?>" class="widefat" name="<?php echo $this->get_field_name( 'subtitle' ); ?>" type="text" value="<?php echo esc_attr( $subtitle ); ?>" />
+			</label>
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'content' ); ?>">
+				<?php _e( 'Content', 'harness-store' ); ?>
+				<textarea id="<?php echo $this->get_field_id( 'content' ); ?>" class="widefat" name="<?php echo $this->get_field_name( 'content' ); ?>" type="text" height="5" value="<?php echo esc_attr( $content ); ?>" />
 			</label>
 		</p>
 		<p>
@@ -72,6 +79,7 @@ class Haste_Woo_Products extends WP_Widget {
 		$instance = array();
 		$instance['title']         = ( ! empty( $new_instance['title'] ) ) ? sanitize_text_field( $new_instance['title'] ) : '';
 		$instance['subtitle']      = ( ! empty( $new_instance['subtitle'] ) ) ? sanitize_text_field( $new_instance['subtitle'] ) : '';
+		$instance['content']      = ( ! empty( $new_instance['content'] ) ) ? sanitize_text_field( $new_instance['content'] ) : '';
 		$instance['shortcode']     = ( ! empty( $new_instance['shortcode'] ) ) ? sanitize_text_field( $new_instance['shortcode'] ) : '';
 
 		return $instance;
@@ -87,6 +95,10 @@ class Haste_Woo_Products extends WP_Widget {
 	 */
 	public function widget( $args, $instance ) {
 
+		echo '<div class="widget-wrapper"><div class="widget-container">';
+
+		echo '<header class="widget-wrapper">';
+
 		$title = apply_filters( 'widget_title', $instance['title'] );
 
 		echo $args['before_widget'];
@@ -96,7 +108,17 @@ class Haste_Woo_Products extends WP_Widget {
 
 		echo '<p class="subtitle">' . $instance['subtitle'] . '</p>';
 
+		echo '</header>';
+
+		echo '<div class="widget-content-wrapper">';
+
+		echo '<div class="widget-content">' . $instance['content'] . '</div>';
+
+		echo '<div class="widget-shortcode">';
+
 		echo do_shortcode( $instance['shortcode'] );
+
+		echo '</div></div></div></div>';
 
 	}
 }
@@ -104,8 +126,8 @@ class Haste_Woo_Products extends WP_Widget {
 /**
  * Register the Like Box Widget.
  */
-function haste_woo_products() {
-	register_widget( 'Haste_Woo_Products' );
+function haste_shortcode_module() {
+	register_widget( 'Haste_Shortcode_Module' );
 }
 
-add_action( 'widgets_init', 'haste_woo_products' );
+add_action( 'widgets_init', 'haste_shortcode_module' );
