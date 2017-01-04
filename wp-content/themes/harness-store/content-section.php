bb7554473561e6a99e4b8463cb7a933b3dfcd187
anyssa <anyssa@hastedesign.com.br>
2017-01-04 01:27:03 -0200

    Limpeza do content da page homeshop

--- wp-content/themes/harness-store/content-section.php
+++ wp-content/themes/harness-store/content-section.php
@@ -12,25 +12,11 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class('container'); ?>>
 	<header class="entry-header">
 		<?php
-			if ( is_single() ) :
-				the_title( '<h1 class="entry-title">', '</h1>' );
-			else :
-				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-			endif;
+				the_title( '<h2 class="entry-title">', '</h2>' );
 		?>
 
-		<?php if ( 'post' == get_post_type() ) : ?>
-			<div class="entry-meta">
-				<?php odin_posted_on(); ?>
-			</div><!-- .entry-meta -->
-		<?php endif; ?>
 	</header><!-- .entry-header -->
 
-	<?php if ( is_search() ) : ?>
-		<div class="entry-summary">
-			<?php the_excerpt(); ?>
-		</div><!-- .entry-summary -->
-	<?php else : ?>
 		<div class="entry-content">
 			<?php
 				the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'harness-store' ) );
@@ -42,15 +28,4 @@
 				) );
 			?>
 		</div><!-- .entry-content -->
-	<?php endif; ?>
-
-	<footer class="entry-meta">
-		<?php if ( in_array( 'category', get_object_taxonomies( get_post_type() ) ) ) : ?>
-			<span class="cat-links"><?php echo __( 'Posted in:', 'harness-store' ) . ' ' . get_the_category_list( _x( ', ', 'Used between list items, there is a space after the comma.', 'harness-store' ) ); ?></span>
-		<?php endif; ?>
-		<?php the_tags( '<span class="tag-links">' . __( 'Tagged as:', 'harness-store' ) . ' ', ', ', '</span>' ); ?>
-		<?php if ( ! post_password_required() && ( comments_open() || get_comments_number() ) ) : ?>
-			<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'harness-store' ), __( '1 Comment', 'harness-store' ), __( '% Comments', 'harness-store' ) ); ?></span>
-		<?php endif; ?>
-	</footer>
 </article><!-- #post-## -->
