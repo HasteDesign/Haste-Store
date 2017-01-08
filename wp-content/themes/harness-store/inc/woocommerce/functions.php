90af449a10184e18dbf099f72f79d7525647aabd
Allyson Alves de Souza <allyson@hastedesign.com.br>
2017-01-08 16:05:08 -0200

    Ajustes na verificação condicional da odin_before_content()

--- wp-content/themes/harness-store/inc/woocommerce/functions.php
+++ wp-content/themes/harness-store/inc/woocommerce/functions.php
@@ -14,7 +14,7 @@ if ( ! function_exists( 'odin_before_content' ) ) {
 	 */
 	function odin_before_content() {
 
-		if ( is_single() || is_checkout() || is_cart() || is_product() || is_home() ) {
+		if ( is_single() || is_home() || ( is_woocommerce_activated() && is_checkout() || is_cart() || is_product() ) ) {
 		?>
 		<main id="content" class="<?php echo odin_classes_page_full(); ?>" tabindex="-1" role="main">
 		<?php
