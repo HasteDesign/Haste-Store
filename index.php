7d500cc491ffc800661388dc64fe924f0ae1ad54
Allyson Souza <Allyson Souza>
2017-01-10 18:03:36 -0200

    Modificado para versionar apenas o tema haste-store

--- index.php
+++ index.php
@@ -1,17 +1,62 @@
 <?php
 /**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
+ * The main template file.
  *
- * @package WordPress
+ * This is the most generic template file in a WordPress theme and one of the
+ * two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * For example, it puts together the home page when no home.php file exists.
+ *
+ * @link http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Odin
+ * @since 2.2.0
  */
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define('WP_USE_THEMES', true);
+get_header(); ?>
 
-/** Loads the WordPress Environment and Template */
-require( dirname( __FILE__ ) . '/wp-blog-header.php' );
+	<main id="content" class="<?php echo get_theme_mod( 'display_blog_sidebar', true )? odin_classes_page_sidebar() : odin_classes_page_full(); ?>" tabindex="-1" role="main">
+
+			<?php
+				if ( have_posts() ) :
+					// Start the Loop.
+					while ( have_posts() ) : the_post();
+
+						/*
+						 * Include the post format-specific template for the content. If you want to
+						 * use this in a child theme, then include a file called called content-___.php
+						 * (where ___ is the post format) and that will be used instead.
+						 */
+						 if ( is_single() ) :
+
+							 get_template_part( 'content', 'section' );
+
+						 else :
+
+							 get_template_part ( 'content', get_post_format() );
+
+						 endif;
+
+					endwhile;
+
+					// Post navigation.
+					odin_paging_nav();
+
+				else :
+					// If no content, include the "No posts found" template.
+					get_template_part( 'content', 'none' );
+
+				endif;
+			?>
+
+	</main><!-- #content -->
+
+<?php
+
+if ( true == get_theme_mod( 'display_blog_sidebar', true ) ) :
+
+	get_sidebar();
+
+endif;
+
+get_footer();
--- wp-content/themes/bazaar-lite/core/admin/assets/js/customizer-preview.js
+++
@@ -1,6 +0,0 @@
-( function( $ ) {
-
-	$('.preview-notice').append('<a class="getpremium" target="_blank" href="' + bazaarlite_details.url + '">' + bazaarlite_details.label + '</a>'); 
-	$('.preview-notice').on("click",function(a){a.stopPropagation()});
-
-} )( jQuery );   
