19d19c7a69b9ee619c43b1682a2e6aa0d58d21f8
anyssa <anyssa@hastedesign.com.br>
2017-02-19 22:28:15 -0300

    New button options: font and border radius

---
+++ inc/kirki/fields/button.php
@@ -0,0 +1,119 @@
+<?php
+
+/**
+ * Buttons section
+ */
+
+ Haste_Store_Kirki::add_section( 'buttons', array(
+    'title'          => __( 'Buttons', 'haste-store' ),
+    'description'    => __( 'Buttons options', 'haste-store' ),
+    'panel'          => '', // Not typically needed.
+    'priority'       => 140,
+    'capability'     => 'edit_theme_options',
+    'theme_supports' => '', // Rarely needed.
+) );
+
+
+Haste_Store_Kirki::add_field( 'haste-store', array(
+	'type'        => 'typography',
+	'settings'    => 'button_type',
+	'label'       => esc_attr__( 'Button typography', 'haste-store' ),
+	'section'     => 'buttons',
+	'default'     => array(
+		'font-family'    => 'Roboto',
+		'variant'        => 'regular',
+		'font-size'      => '14px',
+		'subsets'        => array( 'latin-ext' ),
+		'color'          => '#fff',
+		'letter-spacing' => '0',
+		'text-transform' => 'none',
+	),
+	'priority'	=> 10,
+	'transport'	=> 'postMessage',
+	'js_vars'   => array(
+		array(
+			'element'  => 	'button,
+							.button,
+							.btn,
+							input[type="submit"],
+							input[type="button"],
+							input[type="reset"],
+							.woocommerce a.button,
+							.woocommerce button.button,
+							.woocommerce input.button,
+							.woocommerce #respond input#submit',
+			'function' => 'style',
+		),
+	),
+	'output'	=> array(
+		array(
+			'element' => array(
+							'body',
+							'button',
+							'.button',
+							'.btn',
+							'input[type="submit"]',
+							'input[type="button"]',
+							'input[type="reset"]',
+							'.woocommerce a.button',
+							'.woocommerce button.button',
+							'.woocommerce input.button',
+							'.woocommerce #respond input#submit',
+						),
+		),
+	),
+) );
+
+
+Haste_Store_Kirki::add_field( 'haste-store', array(
+	'type'        => 'slider',
+	'settings'    => 'button_border_radius',
+	'label'       => __( 'Button border radius', 'haste-store' ),
+	'section'     => 'buttons',
+	'default'     => 0,
+	'choices'     => array(
+		'min'  => '0',
+		'max'  => '100',
+		'step' => '1',
+	),
+	'priority'	=> 20,
+	'transport'	=> 'postMessage',
+	'js_vars'   => array(
+		array(
+			'element'  => 	'button,
+							.button,
+							.btn,
+							input[type="submit"],
+							input[type="button"],
+							input[type="reset"],
+							.woocommerce a.button,
+							.woocommerce button.button,
+							.woocommerce input.button,
+							.woocommerce #respond input#submit,
+							.woocommerce #place_order',
+			'function' => 'style',
+			'property' => 'border-radius',
+			'units'    => 'px',
+		),
+	),
+	'output'	=> array(
+		array(
+			'element' => array(
+							'body',
+							'button',
+							'.button',
+							'.btn',
+							'input[type="submit"]',
+							'input[type="button"]',
+							'input[type="reset"]',
+							'.woocommerce a.button',
+							'.woocommerce button.button',
+							'.woocommerce input.button',
+							'.woocommerce #respond input#submit',
+							'.woocommerce #place_order',
+						),
+			'property' => 'border-radius',
+			'units'    => 'px',
+		),
+	),
+) );
--- inc/kirki/kirki-config.php
+++ inc/kirki/kirki-config.php
@@ -9,6 +9,7 @@ Haste_Store_Kirki::add_config( 'haste-store', array(
  */
 
 include_once dirname( __FILE__ ) . '/fields/color.php';
+include_once dirname( __FILE__ ) . '/fields/button.php';
 include_once dirname( __FILE__ ) . '/fields/type.php';
 include_once dirname( __FILE__ ) . '/fields/header.php';
 include_once dirname( __FILE__ ) . '/fields/footer.php';
