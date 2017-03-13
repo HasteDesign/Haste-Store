d02c0860e830fa5eafd8e3e5fb73cea152fd6486
Anyssa Ferreira <anyssa@hastedesign.com.br>
2017-03-13 14:38:39 -0300

    Conditionally hide things when is a landing page

--- footer.php
+++ footer.php
@@ -14,41 +14,43 @@
 	</div><!-- #wrapper -->
 
 	<footer id="footer" role="contentinfo">
-		<section class="footer-widgets sidebar">
-			<div class="container">
-				<div class="row">
+		<?php if ( ! is_page_template( 'page-landing.php' ) ) : ?>
+			<section class="footer-widgets sidebar">
+				<div class="container">
+					<div class="row">
 
-					<div class="footer-widget-area-1 col-md-3 col-sm-6 col-xs-12">
-						<?php
-							if ( ! dynamic_sidebar( 'footer-widget-area-1' ) ) {
-							}
-						?>
+						<div class="footer-widget-area-1 col-md-3 col-sm-6 col-xs-12">
+							<?php
+								if ( ! dynamic_sidebar( 'footer-widget-area-1' ) ) {
+								}
+							?>
+						</div>
+
+						<div class="footer-widget-area-2 col-md-3 col-sm-6 col-xs-12">
+							<?php
+								if ( ! dynamic_sidebar( 'footer-widget-area-2' ) ) {
+								}
+							?>
+						</div>
+
+						<div class="footer-widget-area-3 col-md-3 col-sm-6 col-xs-12">
+							<?php
+								if ( ! dynamic_sidebar( 'footer-widget-area-3' ) ) {
+								}
+							?>
+						</div>
+
+						<div class="footer-widget-area-4 col-md-3 col-sm-6 col-xs-12">
+							<?php
+								if ( ! dynamic_sidebar( 'footer-widget-area-4' ) ) {
+								}
+							?>
+						</div>
+
 					</div>
-
-					<div class="footer-widget-area-2 col-md-3 col-sm-6 col-xs-12">
-						<?php
-							if ( ! dynamic_sidebar( 'footer-widget-area-2' ) ) {
-							}
-						?>
-					</div>
-
-					<div class="footer-widget-area-3 col-md-3 col-sm-6 col-xs-12">
-						<?php
-							if ( ! dynamic_sidebar( 'footer-widget-area-3' ) ) {
-							}
-						?>
-					</div>
-
-					<div class="footer-widget-area-4 col-md-3 col-sm-6 col-xs-12">
-						<?php
-							if ( ! dynamic_sidebar( 'footer-widget-area-4' ) ) {
-							}
-						?>
-					</div>
-
 				</div>
-			</div>
-		</section>
+			</section>
+		<?php endif; // if is landing ?>
 
 		<section class="footer-colophon">
 			<div class="container">
--- header.php
+++ header.php
@@ -57,90 +57,93 @@
 						</div>
 					</div>
 
-					<nav class="collapse navbar-collapse navbar-main-navigation <?php echo is_woocommerce_activated() ? 'navbar-left' : 'navbar-right'; ?>" role="navigation">
-						<?php
-						wp_nav_menu(
-							array(
-								'theme_location' => 'main-menu',
-								'depth'          => 2,
-								'container'      => false,
-								'menu_class'     => 'nav navbar-nav',
-								'fallback_cb'    => 'Odin_Bootstrap_Nav_Walker::fallback',
-								'walker'         => new Odin_Bootstrap_Nav_Walker()
-								)
-							);
-							?>
-						</nav><!-- .navbar-collapse -->
+					<?php if ( ! is_page_template( 'page-landing.php' ) ) : ?>
+						<nav class="collapse navbar-collapse navbar-main-navigation <?php echo is_woocommerce_activated() ? 'navbar-left' : 'navbar-right'; ?>" role="navigation">
+							<?php
+							wp_nav_menu(
+								array(
+									'theme_location' => 'main-menu',
+									'depth'          => 2,
+									'container'      => false,
+									'menu_class'     => 'nav navbar-nav',
+									'fallback_cb'    => 'Odin_Bootstrap_Nav_Walker::fallback',
+									'walker'         => new Odin_Bootstrap_Nav_Walker()
+									)
+								);
+								?>
+							</nav><!-- .navbar-collapse -->
 
-						<?php if( is_woocommerce_activated() ) : ?>
+							<?php if( is_woocommerce_activated() ) : ?>
 
-							<nav class="navbar-right">
+								<nav class="navbar-right">
 
-								<?php if ( get_option( 'users_can_register' ) ) : ?>
+									<?php if ( get_option( 'users_can_register' ) ) : ?>
 
-									<?php if ( is_user_logged_in() ) : ?>
+										<?php if ( is_user_logged_in() ) : ?>
 
-										<div class="btn-group hidden-xs" role="group" aria-label="login">
-											<button class="btn btn-outline navbar-btn dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-												<span class="glyphicon glyphicon-user" aria-hidden="true"></span>
-												<span class="hidden-xs">
-													<?php
-													$user = wp_get_current_user();
-													$username = $user->user_firstname;
-													printf( esc_html__( 'Hello, %s!', 'haste-store' ), $username );
-													?>
-												</span>
-												<span class="caret"></span>
-											</button>
-											<ul class="dropdown-menu">
-												<li>
-													<a href="<?php echo wc_get_endpoint_url( 'orders', '', get_permalink( get_option('woocommerce_myaccount_page_id') ) );?>">
-														<span class="glyphicon glyphicon-th-list" aria-hidden="true"></span>
-														<?php _e('My orders', 'haste-store'); ?>
-													</a>
-												</li>
-												<li>
-													<a href="<?php echo get_permalink( get_option('woocommerce_myaccount_page_id') ); ?>">
-														<span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
-														<?php _e('My account', 'haste-store'); ?></a>
-												</li>
-												<li>
-													<a href="<?php echo wp_logout_url( get_permalink() ); ?>">
-														<span class="glyphicon glyphicon-log-out" aria-hidden="true"></span>
-														<?php _e('Logout', 'haste-store'); ?></a>
-												</li>
-											</ul>
-										</div>
+											<div class="btn-group hidden-xs" role="group" aria-label="login">
+												<button class="btn btn-outline navbar-btn dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+													<span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+													<span class="hidden-xs">
+														<?php
+														$user = wp_get_current_user();
+														$username = $user->user_firstname;
+														printf( esc_html__( 'Hello, %s!', 'haste-store' ), $username );
+														?>
+													</span>
+													<span class="caret"></span>
+												</button>
+												<ul class="dropdown-menu">
+													<li>
+														<a href="<?php echo wc_get_endpoint_url( 'orders', '', get_permalink( get_option('woocommerce_myaccount_page_id') ) );?>">
+															<span class="glyphicon glyphicon-th-list" aria-hidden="true"></span>
+															<?php _e('My orders', 'haste-store'); ?>
+														</a>
+													</li>
+													<li>
+														<a href="<?php echo get_permalink( get_option('woocommerce_myaccount_page_id') ); ?>">
+															<span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
+															<?php _e('My account', 'haste-store'); ?></a>
+													</li>
+													<li>
+														<a href="<?php echo wp_logout_url( get_permalink() ); ?>">
+															<span class="glyphicon glyphicon-log-out" aria-hidden="true"></span>
+															<?php _e('Logout', 'haste-store'); ?></a>
+													</li>
+												</ul>
+											</div>
 
-									<?php else : ?>
+										<?php else : ?>
 
-										<p class="navbar-text hidden-xs">
-											<?php _e('Hello, guest user!', 'haste-store'); ?>
-										</p>
+											<p class="navbar-text hidden-xs">
+												<?php _e('Hello, guest user!', 'haste-store'); ?>
+											</p>
 
-										<div class="btn-group hidden-xs" role="group" aria-label="login">
-											<a class="btn btn-outline navbar-btn" href="<?php echo get_permalink( get_option('woocommerce_myaccount_page_id') ); ?>">
-											<?php _e('Login', 'haste-store')?></a>
-											<a class="btn btn-outline navbar-btn" href="<?php echo get_permalink( get_option('woocommerce_myaccount_page_id') ); ?>">
-											<?php _e('Register', 'haste-store')?></a>
-										</div>
+											<div class="btn-group hidden-xs" role="group" aria-label="login">
+												<a class="btn btn-outline navbar-btn" href="<?php echo get_permalink( get_option('woocommerce_myaccount_page_id') ); ?>">
+												<?php _e('Login', 'haste-store')?></a>
+												<a class="btn btn-outline navbar-btn" href="<?php echo get_permalink( get_option('woocommerce_myaccount_page_id') ); ?>">
+												<?php _e('Register', 'haste-store')?></a>
+											</div>
 
 
-									<?php endif; // is_user_logged_in ?>
+										<?php endif; // is_user_logged_in ?>
 
-								<?php endif; // users_can_register ?>
+									<?php endif; // users_can_register ?>
 
-								<button type="button" class="btn btn-primary navbar-btn dropdown-toggle btn-cart" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-									<span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span>
-									<span class="badge"><?php echo WC()->cart->get_cart_contents_count(); ?></span>
-									<span class="sr-only"><?php _e('Cart', 'haste-store')?></span>
-									<span class="caret"></span>
-								</button>
-								<?php odin_header_cart() ?>
+									<button type="button" class="btn btn-primary navbar-btn dropdown-toggle btn-cart" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+										<span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span>
+										<span class="badge"><?php echo WC()->cart->get_cart_contents_count(); ?></span>
+										<span class="sr-only"><?php _e('Cart', 'haste-store')?></span>
+										<span class="caret"></span>
+									</button>
+									<?php odin_header_cart() ?>
 
-							</nav><!-- .navbar-right -->
+								</nav><!-- .navbar-right -->
 
-						<?php endif; // is_woocommerce_activated ?>
+							<?php endif; // is_woocommerce_activated ?>
+
+						<?php endif; // is_landing ?>
 
 					</div><!-- .container-->
 
@@ -151,12 +154,12 @@
 			<div id="wrapper">
 				<div class="wrapper-row">
 
-					<?php if( !is_front_page() || !is_home() || !is_page_template( 'page-home.php' ) ) : ?>
+					<?php if( !is_front_page() || !is_home() || !is_page_template( 'page-home.php' ) || !is_page_template( 'page-landing.php' ) ) : ?>
 						<div class="col-md-12">
 					<?php endif; ?>
 
 							<?php do_action( 'odin_content_top' ); ?>
 
-					<?php if( !is_front_page() || !is_home() || !is_page_template( 'page-home.php' ) ) : ?>
+					<?php if( !is_front_page() || !is_home() || !is_page_template( 'page-home.php' ) || !is_page_template( 'page-landing.php' ) ) : ?>
 						</div>
 					<?php endif; ?>
