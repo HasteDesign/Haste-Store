a166684b5dbec0bea757ab9761a00896379a6c9c
Allyson Souza <allyson@hastedesign.com.br>
2017-03-25 21:25:59 -0300

    Changed textdomain from haste widgets modules

--- core/classes/widgets/class-haste-content-image-module.php
+++ core/classes/widgets/class-haste-content-image-module.php
@@ -4,7 +4,7 @@
  *
  * Facebook like widget.
  *
- * @package  Haste-Widgets-Modules
+ * @package  haste-store
  * @category Widget
  * @author   Anyssa Ferreira
  * @version  1.0
@@ -17,8 +17,8 @@ class Haste_Content_Image_Module extends WP_Widget {
 	public function __construct() {
 		parent::__construct(
 			'haste_content_image_module',
-			__( 'Haste Content and Image Module', 'haste-widgets-modules' ),
-			array( 'description' => __( 'Displays title, content (shotcodes included), a button and image as background, with few options.', 'haste-widgets-modules' ), 'customize_selective_refresh' => true, )
+			__( 'Haste Content and Image Module', 'haste-store' ),
+			array( 'description' => __( 'Displays title, content (shotcodes included), a button and image as background, with few options.', 'haste-store' ), 'customize_selective_refresh' => true, )
 		);
 	}
 
@@ -32,7 +32,7 @@ class Haste_Content_Image_Module extends WP_Widget {
 	 * @return string          Widget options form.
 	 */
 	public function form( $instance ) {
-		$title         = isset( $instance['title'] ) ? $instance['title'] : __('My module title', 'haste-widgets-modules');
+		$title         = isset( $instance['title'] ) ? $instance['title'] : __('My module title', 'haste-store');
 		$subtitle      = isset( $instance['subtitle'] ) ? $instance['subtitle'] : '';
 		$content       = isset( $instance['content'] ) ? $instance['content'] : '';
 		$text_color    = isset( $instance['text_color'] ) ? $instance['text_color'] : '';
@@ -51,149 +51,149 @@ class Haste_Content_Image_Module extends WP_Widget {
 		$bg_color      = isset( $instance['bg_color'] ) ? $instance['bg_color'] : '#eeeeee';
 
 		?>
-		<h3><?php _e( 'Content options', 'haste-widgets-modules' ); ?></h3>
+		<h3><?php _e( 'Content options', 'haste-store' ); ?></h3>
 		<p>
 			<label for="<?php echo $this->get_field_id( 'title' ); ?>">
-				<?php _e( 'Title:', 'haste-widgets-modules' ); ?>
+				<?php _e( 'Title:', 'haste-store' ); ?>
 				<input id="<?php echo $this->get_field_id( 'title' ); ?>" class="widefat" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
 			</label>
 		</p>
 		<p>
 			<label for="<?php echo $this->get_field_id( 'subtitle' ); ?>">
-				<?php _e( 'Subtitle', 'haste-widgets-modules' ); ?>
+				<?php _e( 'Subtitle', 'haste-store' ); ?>
 				<input id="<?php echo $this->get_field_id( 'subtitle' ); ?>" class="widefat" name="<?php echo $this->get_field_name( 'subtitle' ); ?>" type="text" value="<?php echo esc_attr( $subtitle ); ?>" />
 			</label>
 		</p>
 		<p>
 			<label for="<?php echo $this->get_field_id( 'content' ); ?>">
-				<?php _e( 'Content', 'haste-widgets-modules' ); ?>
+				<?php _e( 'Content', 'haste-store' ); ?>
 				<textarea id="<?php echo $this->get_field_id( 'content' ); ?>" class="widefat" name="<?php echo $this->get_field_name( 'content' ); ?>" rows="5"><?php echo esc_attr( $content ); ?></textarea>
 			</label>
 		</p>
 		<p>
             <label for="<?php echo $this->get_field_id( 'text_color' ); ?>">
-				<?php _e( 'Text Color', 'haste-widgets-modules' ); ?>
+				<?php _e( 'Text Color', 'haste-store' ); ?>
 			</label>
             <input class="wp-color-picker widefat" type="text" id="<?php echo $this->get_field_id( 'text_color' ); ?>" name="<?php echo $this->get_field_name( 'text_color' ); ?>" value="<?php echo esc_attr( $text_color ); ?>" />
         </p>
 		<p>
 			<label for="<?php echo $this->get_field_id( 'btn_text' ); ?>">
-				<?php _e( 'Button text', 'haste-widgets-modules' ); ?>
+				<?php _e( 'Button text', 'haste-store' ); ?>
 				<input id="<?php echo $this->get_field_id( 'btn_text' ); ?>" class="widefat" name="<?php echo $this->get_field_name( 'btn_text' ); ?>" type="text" value="<?php echo esc_attr( $btn_text ); ?>" />
 			</label>
 		</p>
 		<p>
-			<label for="<?php echo $this->get_field_name( 'btn_link' ); ?>"><?php _e( 'Button link:', 'haste-widgets-modules' ); ?>
+			<label for="<?php echo $this->get_field_name( 'btn_link' ); ?>"><?php _e( 'Button link:', 'haste-store' ); ?>
 				<input name="<?php echo $this->get_field_name( 'btn_link' ); ?>" id="<?php echo $this->get_field_id( 'btn_link' ); ?>" class="widefat" type="text" value="<?php echo esc_attr( $btn_link ); ?>" />
 			</label>
 		</p>
 		<p>
 			<label for="<?php echo $this->get_field_id( 'btn_class' ); ?>">
-				<?php _e( 'Button CSS classes', 'haste-widgets-modules' ); ?>
+				<?php _e( 'Button CSS classes', 'haste-store' ); ?>
 				<input id="<?php echo $this->get_field_id( 'btn_class' ); ?>" class="widefat" name="<?php echo $this->get_field_name( 'btn_class' ); ?>" type="text" value="<?php echo esc_attr( $btn_class ); ?>" />
 			</label>
 		</p>
 		<p>
 			<label for="<?php echo $this->get_field_id( 'align' ); ?>">
 
-				<?php _e( 'Content alignment', 'haste-widgets-modules' ); ?>
+				<?php _e( 'Content alignment', 'haste-store' ); ?>
 
 				<select class='widefat' id="<?php echo $this->get_field_id('align'); ?>"
 					name="<?php echo $this->get_field_name( 'align' ); ?>" type="text">
 					<option
 						value='content-center'
 						<?php echo ( $align == 'content-center' ) ? 'selected' : ''; ?>>
-						<?php _e( 'Content centered', 'haste-widgets-modules' ); ?>
+						<?php _e( 'Content centered', 'haste-store' ); ?>
 					</option>
 					<option
 						value='content-left'
 						<?php echo ( $align == 'content-left' ) ? 'selected' : ''; ?>>
-						<?php _e( 'Content on left', 'haste-widgets-modules' ); ?>
+						<?php _e( 'Content on left', 'haste-store' ); ?>
 					</option>
 					<option
 						value='content-right'
 						<?php echo ( $align == 'content-right' ) ? 'selected' : ''; ?>>
-						<?php _e( 'Content on right', 'haste-widgets-modules' ); ?>
+						<?php _e( 'Content on right', 'haste-store' ); ?>
 					</option>
 				</select>
 			</label>
 		</p>
 		<p>
 			<label for="<?php echo $this->get_field_id( 'padding' ); ?>">
-				<?php _e( 'Area padding (%)', 'haste-widgets-modules' ); ?>
+				<?php _e( 'Area padding (%)', 'haste-store' ); ?>
 				<input id="<?php echo $this->get_field_id( 'padding' ); ?>" class="widefat" name="<?php echo $this->get_field_name( 'padding' ); ?>" type="number" min="0" max="100" value="<?php echo esc_attr( $padding ); ?>" />
 			</label>
 		</p>
 		<hr />
-		<h3><?php _e( 'Background options', 'haste-widgets-modules' ); ?></h3>
+		<h3><?php _e( 'Background options', 'haste-store' ); ?></h3>
 		<p class="upload-img">
 			<div class='image-preview-wrapper'>
 				<img class='image-preview' src='<?php echo esc_attr( $image ); ?>' width='100%' height='100' style='max-height: 100px; width: 100px;'>
 			</div>
-			<label for="<?php echo $this->get_field_name( 'image' ); ?>"><?php _e( 'Image:', 'haste-widgets-modules' ); ?></label>
+			<label for="<?php echo $this->get_field_name( 'image' ); ?>"><?php _e( 'Image:', 'haste-store' ); ?></label>
 			<input class="image_attachment_id widefat" name="<?php echo $this->get_field_name( 'image' ); ?>" id="<?php echo $this->get_field_id( 'image' ); ?>" type="text" size="36"  value="<?php echo esc_attr( $image ); ?>" />
-			<input class="upload_image_button" type="button" value="<?php _e( 'Set image ', 'haste-widgets-modules' ); ?>" />
-			<input class="clear_image_button" type="button" value="<?php _e( 'Remove', 'haste-widgets-modules' ); ?>" />
+			<input class="upload_image_button" type="button" value="<?php _e( 'Set image ', 'haste-store' ); ?>" />
+			<input class="clear_image_button" type="button" value="<?php _e( 'Remove', 'haste-store' ); ?>" />
 		</p>
 		<p>
 			<label for="<?php echo $this->get_field_id( 'img_pos_v' ); ?>">
-				<?php _e( 'Image vertical position (%)', 'haste-widgets-modules' ); ?>
+				<?php _e( 'Image vertical position (%)', 'haste-store' ); ?>
 				<input id="<?php echo $this->get_field_id( 'img_pos_v' ); ?>" class="widefat" name="<?php echo $this->get_field_name( 'img_pos_v' ); ?>" type="number" min="-50" max="150" value="<?php echo esc_attr( $img_pos_v ); ?>" />
 			</label>
 		</p>
 		<p>
 			<label for="<?php echo $this->get_field_id( 'img_pos_h' ); ?>">
-				<?php _e( 'Image horizontal position (%)', 'haste-widgets-modules' ); ?>
+				<?php _e( 'Image horizontal position (%)', 'haste-store' ); ?>
 				<input id="<?php echo $this->get_field_id( 'img_pos_h' ); ?>" class="widefat" name="<?php echo $this->get_field_name( 'img_pos_h' ); ?>" type="number" min="-50" max="150" value="<?php echo esc_attr( $img_pos_h ); ?>" />
 			</label>
 		</p>
 		<p>
 			<label for="<?php echo $this->get_field_id( 'img_size' ); ?>">
-				<?php _e( 'Image size (%)', 'haste-widgets-modules' ); ?>
+				<?php _e( 'Image size (%)', 'haste-store' ); ?>
 				<input id="<?php echo $this->get_field_id( 'img_size' ); ?>" class="widefat" name="<?php echo $this->get_field_name( 'img_size' ); ?>" type="number" min="-50" max="150" value="<?php echo esc_attr( $img_size ); ?>" />
 			</label>
 			<label for="<?php echo $this->get_field_id( 'cover' ); ?>">
-				<input id="<?php echo $this->get_field_id( 'cover' ); ?>" name="<?php echo $this->get_field_name( 'cover' ); ?>" type="checkbox" value="1" <?php checked( 1, $cover, true ); ?> /> <?php _e( 'Expand image to cover the entire module area', 'haste-widgets-modules' ); ?>
+				<input id="<?php echo $this->get_field_id( 'cover' ); ?>" name="<?php echo $this->get_field_name( 'cover' ); ?>" type="checkbox" value="1" <?php checked( 1, $cover, true ); ?> /> <?php _e( 'Expand image to cover the entire module area', 'haste-store' ); ?>
 			</label>
 		</p>
 		<p>
 			<label for="<?php echo $this->get_field_id( 'bg_fix' ); ?>">
-				<input id="<?php echo $this->get_field_id( 'bg_fix' ); ?>" name="<?php echo $this->get_field_name( 'bg_fix' ); ?>" type="checkbox" value="1" <?php checked( 1, $cover, true ); ?> /> <?php _e( 'Fix the background image', 'haste-widgets-modules' ); ?>
+				<input id="<?php echo $this->get_field_id( 'bg_fix' ); ?>" name="<?php echo $this->get_field_name( 'bg_fix' ); ?>" type="checkbox" value="1" <?php checked( 1, $cover, true ); ?> /> <?php _e( 'Fix the background image', 'haste-store' ); ?>
 			</label>
 		</p>
 		<p>
 			<label for="<?php echo $this->get_field_id( 'bg_repeat' ); ?>">
 
-				<?php _e( 'Background image repeat', 'haste-widgets-modules' ); ?>
+				<?php _e( 'Background image repeat', 'haste-store' ); ?>
 
 				<select class='widefat' id="<?php echo $this->get_field_id('bg_repeat'); ?>"
 					name="<?php echo $this->get_field_name( 'bg_repeat' ); ?>" type="text">
 					<option
 						value='no-repeat'
 						<?php echo ( $align =='no-repeat' ) ? 'selected' : ''; ?>>
-						<?php _e( 'No repeat', 'haste-widgets-modules' ); ?>
+						<?php _e( 'No repeat', 'haste-store' ); ?>
 					</option>
 					<option
 						value='repeat'
 						<?php echo ( $align =='repeat' ) ? 'selected' : ''; ?>>
-						<?php _e( 'Vertical and horizontal repeat', 'haste-widgets-modules' ); ?>
+						<?php _e( 'Vertical and horizontal repeat', 'haste-store' ); ?>
 					</option>
 					<option
 						value='repeat-x'
 						<?php echo ( $align =='repeat-x' ) ? 'selected' : ''; ?>>
-						<?php _e( 'Horizontal repeat', 'haste-widgets-modules' ); ?>
+						<?php _e( 'Horizontal repeat', 'haste-store' ); ?>
 					</option>
 					<option
 						value='repeat-y'
 						<?php echo ( $align =='repeat-y' ) ? 'selected' : ''; ?>>
-						<?php _e( 'Vertical repeat', 'haste-widgets-modules' ); ?>
+						<?php _e( 'Vertical repeat', 'haste-store' ); ?>
 					</option>
 				</select>
 			</label>
 		</p>
 		<p>
             <label for="<?php echo $this->get_field_id( 'bg_color' ); ?>">
-				<?php _e( 'Background Color', 'haste-widgets-modules' ); ?>
+				<?php _e( 'Background Color', 'haste-store' ); ?>
 			</label>
             <input class="wp-color-picker widefat" type="text" id="<?php echo $this->get_field_id( 'bg_color' ); ?>" name="<?php echo $this->get_field_name( 'bg_color' ); ?>" value="<?php echo esc_attr( $bg_color ); ?>" />
         </p>
@@ -242,7 +242,7 @@ class Haste_Content_Image_Module extends WP_Widget {
 	 * @return string            Facebook like box.
 	 */
 	public function widget( $args, $instance ) {
-		$title         = isset( $instance['title'] ) ? $instance['title'] : __( 'My module title', 'haste-widgets-modules' );
+		$title         = isset( $instance['title'] ) ? $instance['title'] : __( 'My module title', 'haste-store' );
 		$subtitle      = isset( $instance['subtitle'] ) ? $instance['subtitle'] : '';
 		$content       = isset( $instance['content'] ) ? $instance['content'] : '';
 		$text_color    = isset( $instance['text_color'] ) ? $instance['text_color'] : '';
--- core/classes/widgets/class-haste-posts-module.php
+++ core/classes/widgets/class-haste-posts-module.php
@@ -2,7 +2,7 @@
 /**
 * Haste_Posts_Module class.
 *
-* @package  Haste-Widgets-Modules
+* @package  haste-store
 * @category Widget
 * @author   Anyssa Ferreira
 * @version  1.0
@@ -15,8 +15,8 @@ class Haste_Posts_Module extends WP_Widget {
 	public function __construct() {
 		parent::__construct(
 			'haste_posts_module',
-			__( 'Haste Posts Module', 'haste-widgets-modules' ),
-			array( 'description' => __( 'Display blog posts.', 'haste-widgets-modules' ), 'customize_selective_refresh' => true, )
+			__( 'Haste Posts Module', 'haste-store' ),
+			array( 'description' => __( 'Display blog posts.', 'haste-store' ), 'customize_selective_refresh' => true, )
 		);
 	}
 
@@ -30,7 +30,7 @@ class Haste_Posts_Module extends WP_Widget {
 	* @return string          Widget options form.
 	*/
 	public function form( $instance ) {
-		$title         	= isset( $instance['title'] ) ? $instance['title'] : __('My module title', 'haste-widgets-modules');
+		$title         	= isset( $instance['title'] ) ? $instance['title'] : __('My module title', 'haste-store');
 		$subtitle      	= isset( $instance['subtitle'] ) ? $instance['subtitle'] : '';
 		$posts_per_page = isset( $instance['posts_per_page'] ) ? $instance['posts_per_page'] : 4;
 		$category		= isset( $instance['category'] ) ? $instance['category'] : '';
@@ -46,56 +46,56 @@ class Haste_Posts_Module extends WP_Widget {
 		?>
 		<p>
 			<label for="<?php echo $this->get_field_id( 'title' ); ?>">
-				<?php _e( 'Title:', 'haste-widgets-modules' ); ?>
+				<?php _e( 'Title:', 'haste-store' ); ?>
 				<input id="<?php echo $this->get_field_id( 'title' ); ?>" class="widefat" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
 			</label>
 		</p>
 		<p>
 			<label for="<?php echo $this->get_field_id( 'subtitle' ); ?>">
-				<?php _e( 'Subtitle', 'haste-widgets-modules' ); ?>
+				<?php _e( 'Subtitle', 'haste-store' ); ?>
 				<input id="<?php echo $this->get_field_id( 'subtitle' ); ?>" class="widefat" name="<?php echo $this->get_field_name( 'subtitle' ); ?>" type="text" value="<?php echo esc_attr( $subtitle ); ?>" />
 			</label>
 		</p>
 		<p>
 			<label for="<?php echo $this->get_field_id( 'posts_per_page' ); ?>">
-				<?php _e( 'Number of posts to display (use -1 to show all posts)', 'haste-widgets-modules' ); ?>
+				<?php _e( 'Number of posts to display (use -1 to show all posts)', 'haste-store' ); ?>
 				<input id="<?php echo $this->get_field_id( 'posts_per_page' ); ?>" class="widefat" name="<?php echo $this->get_field_name( 'posts_per_page' ); ?>" type="number" min="-1" max="50" value="<?php echo esc_attr( $posts_per_page ); ?>" />
 			</label>
 		</p>
-		<h3><?php _e( 'Post selection criteria', 'haste-widgets-modules' ); ?></h3>
+		<h3><?php _e( 'Post selection criteria', 'haste-store' ); ?></h3>
 		<p>
 			<label for="<?php echo $this->get_field_id( 'category' ); ?>">
-				<?php _e( 'From a category (category slug)', 'haste-widgets-modules' ); ?>
+				<?php _e( 'From a category (category slug)', 'haste-store' ); ?>
 				<input id="<?php echo $this->get_field_id( 'category' ); ?>" class="widefat" name="<?php echo $this->get_field_name( 'category' ); ?>" type="text" value="<?php echo esc_attr( $category ); ?>" />
 			</label>
 		</p>
 		<p>
 			<label for="<?php echo $this->get_field_id( 'keyword' ); ?>">
-				<?php _e( 'From keyword', 'haste-widgets-modules' ); ?>
+				<?php _e( 'From keyword', 'haste-store' ); ?>
 				<input id="<?php echo $this->get_field_id( 'keyword' ); ?>" class="widefat" name="<?php echo $this->get_field_name( 'keyword' ); ?>" type="text" value="<?php echo esc_attr( $keyword ); ?>" />
 			</label>
 		</p>
 		<p>
 			<label for="<?php echo $this->get_field_id( 'order_by' ); ?>">
 
-				<?php _e( 'Order posts by', 'haste-widgets-modules' ); ?>
+				<?php _e( 'Order posts by', 'haste-store' ); ?>
 
 				<select class='widefat' id="<?php echo $this->get_field_id('order_by'); ?>"
 					name="<?php echo $this->get_field_name( 'order_by' ); ?>">
 					<option
 						value='date'
 						<?php echo ( $order_by == 'date' ) ? 'selected' : ''; ?>>
-						<?php _e( 'Date', 'haste-widgets-modules' ); ?>
+						<?php _e( 'Date', 'haste-store' ); ?>
 					</option>
 					<option
 						value='title'
 						<?php echo ( $order_by == 'title' ) ? 'selected' : ''; ?>>
-						<?php _e( 'Title', 'haste-widgets-modules' ); ?>
+						<?php _e( 'Title', 'haste-store' ); ?>
 					</option>
 					<option
 						value='rand'
 						<?php echo ( $order_by == 'rand' ) ? 'selected' : ''; ?>>
-						<?php _e( 'Random', 'haste-widgets-modules' ); ?>
+						<?php _e( 'Random', 'haste-store' ); ?>
 					</option>
 				</select>
 			</label>
@@ -103,43 +103,43 @@ class Haste_Posts_Module extends WP_Widget {
 		<p>
 			<label for="<?php echo $this->get_field_id( 'order' ); ?>">
 
-				<?php _e( 'Order', 'haste-widgets-modules' ); ?>
+				<?php _e( 'Order', 'haste-store' ); ?>
 
 				<select class='widefat' id="<?php echo $this->get_field_id('order'); ?>"
 					name="<?php echo $this->get_field_name( 'order' ); ?>">
 					<option
 						value='DESC'
 						<?php echo ( $order == 'DESC' ) ? 'selected' : ''; ?>>
-						<?php _e( 'Descending', 'haste-widgets-modules' ); ?>
+						<?php _e( 'Descending', 'haste-store' ); ?>
 					</option>
 					<option
 						value='ASC'
 						<?php echo ( $order == 'ASC' ) ? 'selected' : ''; ?>>
-						<?php _e( 'Ascending', 'haste-widgets-modules' ); ?>
+						<?php _e( 'Ascending', 'haste-store' ); ?>
 					</option>
 				</select>
 			</label>
 		</p>
 		<p>
 			<label for="<?php echo $this->get_field_id( 'posts_classes' ); ?>">
-				<?php _e( 'CSS posts classes', 'haste-widgets-modules' ); ?>
+				<?php _e( 'CSS posts classes', 'haste-store' ); ?>
 				<input id="<?php echo $this->get_field_id( 'posts_classes' ); ?>" class="widefat" name="<?php echo $this->get_field_name( 'posts_classes' ); ?>" type="text" value="<?php echo esc_attr( $posts_classes ); ?>" />
 			</label>
 		</p>
-		<h3><?php _e( 'Post display options', 'haste-widgets-modules' ); ?></h3>
+		<h3><?php _e( 'Post display options', 'haste-store' ); ?></h3>
 		<p>
 			<label for="<?php echo $this->get_field_id( 'display_thumb' ); ?>">
-				<input id="<?php echo $this->get_field_id( 'display_thumb' ); ?>" name="<?php echo $this->get_field_name( 'display_thumb' ); ?>" type="checkbox" value="1" <?php checked( 1, $display_thumb, true ); ?> /> <?php _e( 'Display post thumbnails', 'haste-widgets-modules' ); ?>
+				<input id="<?php echo $this->get_field_id( 'display_thumb' ); ?>" name="<?php echo $this->get_field_name( 'display_thumb' ); ?>" type="checkbox" value="1" <?php checked( 1, $display_thumb, true ); ?> /> <?php _e( 'Display post thumbnails', 'haste-store' ); ?>
 			</label>
 		</p>
 		<p>
 			<label for="<?php echo $this->get_field_id( 'display_excerpt' ); ?>">
-				<input id="<?php echo $this->get_field_id( 'display_excerpt' ); ?>" name="<?php echo $this->get_field_name( 'display_excerpt' ); ?>" type="checkbox" value="1" <?php checked( 1, $display_excerpt, true ); ?> /> <?php _e( 'Display post excerpts', 'haste-widgets-modules' ); ?>
+				<input id="<?php echo $this->get_field_id( 'display_excerpt' ); ?>" name="<?php echo $this->get_field_name( 'display_excerpt' ); ?>" type="checkbox" value="1" <?php checked( 1, $display_excerpt, true ); ?> /> <?php _e( 'Display post excerpts', 'haste-store' ); ?>
 			</label>
 		</p>
 		<p>
 			<label for="<?php echo $this->get_field_id( 'display_date' ); ?>">
-				<input id="<?php echo $this->get_field_id( 'display_date' ); ?>" name="<?php echo $this->get_field_name( 'display_date' ); ?>" type="checkbox" value="1" <?php checked( 1, $display_date, true ); ?> /> <?php _e( 'Display post dates', 'haste-widgets-modules' ); ?>
+				<input id="<?php echo $this->get_field_id( 'display_date' ); ?>" name="<?php echo $this->get_field_name( 'display_date' ); ?>" type="checkbox" value="1" <?php checked( 1, $display_date, true ); ?> /> <?php _e( 'Display post dates', 'haste-store' ); ?>
 			</label>
 		</p>
 
@@ -184,7 +184,7 @@ class Haste_Posts_Module extends WP_Widget {
 	* @return string            Facebook like box.
 	*/
 	public function widget( $args, $instance ) {
-        $title         	= isset( $instance['title'] ) ? $instance['title'] : __( 'My module title', 'haste-widgets-modules' );
+        $title         	= isset( $instance['title'] ) ? $instance['title'] : __( 'My module title', 'haste-store' );
         $subtitle      	= isset( $instance['subtitle'] ) ? $instance['subtitle'] : '';
         $posts_per_page = isset( $instance['posts_per_page'] ) ? $instance['posts_per_page'] : 4;
         $category		= isset( $instance['category'] ) ? $instance['category'] : '';
@@ -284,7 +284,7 @@ class Haste_Posts_Module extends WP_Widget {
 			wp_reset_postdata();
 
 			else :
-				 _e( 'Sorry, no posts matched your criteria.', 'haste-widgets-modules' );
+				 _e( 'Sorry, no posts matched your criteria.', 'haste-store' );
 
 			endif;
 
